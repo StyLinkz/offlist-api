75dafc2c773a44a1b6b6bde4d3409d121c4391bc
ducnm <ducitbk@gmail.com>
2022-10-04 08:54:11 +0700

    feat: allow admin to manage translations of multi languages

---
+++ database/migrations/2022_10_02_162518_create_language_translations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguageTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language_translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('language_id');
+            $table->bigInteger('translation_id');
+            $table->string('translation_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('language_translations');
+    }
+}
